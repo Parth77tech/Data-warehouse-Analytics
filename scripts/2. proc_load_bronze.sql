72c355575393309b88a0ccfc05a3b232a8beaada
Parth Tailor <55438214+Parth77tech@users.noreply.github.com>
2025-05-25 12:00:08 -0400

    Update 2. proc_load_bronze.sql

--- scripts/2. proc_load_bronze.sql
+++ scripts/2. proc_load_bronze.sql
@@ -15,7 +15,7 @@ BEGIN
 	PRINT '=====================================';
 	TRUNCATE TABLE bronze.crm_cust_info;
 	BULK INSERT bronze.crm_cust_info
-	FROM 'D:\MS SQL SERVER\Data With Barra\0. sql-data-warehouse-project\datasets\source_crm\cust_info.csv'
+	FROM 'D:\datasets\source_crm\cust_info.csv'
 	WITH (
 		FIRSTROW = 2,
 		FIELDTERMINATOR = ',',
@@ -28,7 +28,7 @@ BEGIN
 
 	TRUNCATE TABLE bronze.crm_prd_info;
 	BULK INSERT bronze.crm_prd_info
-	FROM 'D:\MS SQL SERVER\Data With Barra\0. sql-data-warehouse-project\datasets\source_crm\prd_info.csv'
+	FROM 'D:\datasets\source_crm\prd_info.csv'
 	WITH (
 		FIRSTROW = 2,
 		FIELDTERMINATOR = ',',
@@ -41,7 +41,7 @@ BEGIN
 
 	TRUNCATE TABLE bronze.crm_sales_details;
 	BULK INSERT bronze.crm_sales_details
-	FROM 'D:\MS SQL SERVER\Data With Barra\0. sql-data-warehouse-project\datasets\source_crm\sales_details.csv'
+	FROM 'D:\datasets\source_crm\sales_details.csv'
 	WITH (
 		FIRSTROW = 2,
 		FIELDTERMINATOR = ',',
@@ -56,7 +56,7 @@ BEGIN
 	PRINT '=====================================';
 	TRUNCATE TABLE bronze.erp_cust_az12;
 	BULK INSERT bronze.erp_cust_az12
-	FROM 'D:\MS SQL SERVER\Data With Barra\0. sql-data-warehouse-project\datasets\source_erp\cust_az12.csv'
+	FROM 'D:\datasets\source_erp\cust_az12.csv'
 	WITH (
 		FIRSTROW = 2, 
 		FIELDTERMINATOR = ',',
@@ -69,7 +69,7 @@ BEGIN
 
 	TRUNCATE TABLE bronze.erp_loc_a101;
 	BULK INSERT bronze.erp_loc_a101
-	FROM 'D:\MS SQL SERVER\Data With Barra\0. sql-data-warehouse-project\datasets\source_erp\loc_a101.csv'
+	FROM 'D:\datasets\source_erp\loc_a101.csv'
 	WITH (
 		FIRSTROW = 2,
 		FIELDTERMINATOR = ',',
@@ -83,7 +83,7 @@ BEGIN
 
 	TRUNCATE TABLE bronze.erp_px_cat_gv12;
 	BULK INSERT bronze.erp_px_cat_gv12
-	FROM 'D:\MS SQL SERVER\Data With Barra\0. sql-data-warehouse-project\datasets\source_erp\px_cat_g1v2.csv'
+	FROM 'D:\datasets\source_erp\px_cat_g1v2.csv'
 	WITH (
 		FIRSTROW = 2, 
 		FIELDTERMINATOR = ',',
